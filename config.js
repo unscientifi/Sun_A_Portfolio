9cff0432ac78bbc8de627f98455a3f1e5a73df16
unscientifi <a_sun69708@fanshaweonline.ca>
2019-12-08 18:51:17 -0500

    update config2

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const config = {
     // port: 8889, // 8889 for mac
     uname: "be3904e8274ec8",
     pword: "c0d53d8a", // leave blank for windows
-    database: "portfolio"
+    database: "heroku_a9e8b17bb040788"
 }
 
 module.exports = config;
