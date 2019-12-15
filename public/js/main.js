de3be66ed748932a8966391292dda0e8cc29e80a
unscientifi <a_sun69708@fanshaweonline.ca>
2019-12-15 15:27:50 -0500

    pic

--- public/js/main.js
+++ public/js/main.js
@@ -30,7 +30,7 @@ const myVM = (() => {
     function getUserData(event) {
         event.preventDefault(); 
         
-        let imgSrc = this.previousElementSibling.getAttribute('src');
+        let imgSrc = this.getAttribute('src');
         let url = `/users/${this.getAttribute('href')}`;  
 
         fetch(url) 
