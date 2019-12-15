eb1a92b75c116f51245980013087fe8907ad0d0d
unscientifi <a_sun69708@fanshaweonline.ca>
2019-12-14 22:57:34 -0500

    update video

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
+const nodemailer = require('nodemailer');
 
 // set the port
 const port = process.env.PORT || 3000;
@@ -13,27 +14,62 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', require('./routes/index'));
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
+// app.get('/', function (req, res) {
+//     res.render('index');
+// });
 
-app.get('/about', function (req, res) {
-    res.render('about');
-});
+// app.get('/about', function (req, res) {
+//     res.render('about');
+// });
 
-app.get('/portfolio', function (req, res) {
-    res.render('portfolio');
-});
+// app.get('/portfolio', function (req, res) {
+//     res.render('portfolio');
+// });
 
-app.get('/contact', function (req, res) {
+app.get('/send', function (req, res) {
     res.render('contact');
 });
 
+app.post('/send', function (req, res) {
 
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'gmail',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: 'ryansunportfolio@gmail.com', // generated ethereal user
+        pass: 'RyanSun11!'  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+});
 
-app.get('/contact.php', (req, res) => {
-    res.sendFile(path.join(__dirname + "/views/contact.php"));
-})
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Ryan Sun" <ryansunportfolio@gmail.com>', // sender address
+      to: 'ryansun1111@gmail.com', // list of receivers
+      subject: 'Nodemailer Test', // Subject line
+      text: 'Hello', // plain text body
+      html: '<b>Thanks for your message. I will get back to you within 24 hours.</b>' // html body
+    };
+
+//   console.log("Message sent: %s", info.messageId);
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email has been sent: %s ' + info.messageId);
+    }
+  });  
+  
+
+res.render('contact', { msg:'Email has been sent' });
+
+});
+
 
 //Error page 
 app.use((req, res, next) => {
--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,23 @@
+// this is a partially revealing module pattern - just a variation on what we've already done
 
 const myVM = (() => {
+    // get the user buttons and fire off an async DB query with Fetch
     let userButtons = document.querySelectorAll('.u-link'),
         lightbox = document.querySelector('.lightbox');
 
-
+   
     function parseUserData(work) { 
         let targetDiv = document.querySelector('.lb-content'),
-            targetImg = lightbox.querySelector('img');
+            targetImg = lightbox.querySelectorAll('.projectImg');
 
         let workContent = `
-            
-            <h3>${work.Category}</h3>
-            <p>Description:</p>
-            <h4>${work.Description}</h4>
-            <video>
-            <source src="${work.Video}.mp4">
-           </video>
+        <h3>${work.Category}</h3>
+        <p></p>
+        <h4>${work.Description}</h4>
+        <p></p>
+        <p>${work.Information}</p>
+        <p>${work.Collaborate}</p>
+        <video controls src="images/${work.Video}" width="80%"></video>
             `;
 
         console.log(workContent);
@@ -27,13 +29,16 @@ const myVM = (() => {
     }
 
     function getUserData(event) {
-        event.preventDefault(); 
-        
-        let imgSrc = this.getAttribute('src');
-        let url = `/users/${this.getAttribute('href')}`;  
+        event.preventDefault(); //kill the default a tag behaviour (don't navigate anythere)
 
-        fetch(url) 
-            .then(res => res.json())  
+        //find this image closets to the anchor tag and get its src property
+        let imgSrc = document.getElementsByClassName('projectImg')[0].getAttribute('src');
+        // document.getElementsByClassName('projectImg')[0].getAttribute('src');
+        let url = `/users/${this.getAttribute('href')}`;  // /1
+
+
+        fetch(url) // go get the data
+            .then(res => res.json())  // parse the json result into a plain object
             .then (data => {
                 console.log("my database result is:", data)
 
@@ -45,8 +50,10 @@ const myVM = (() => {
                 console.log(err)
             });
 
+
     }
 
+    
     userButtons.forEach(button => button.addEventListener('click', getUserData));
     lightbox.querySelector('.close').addEventListener('click', function () {
         lightbox.classList.remove('show-lb');
